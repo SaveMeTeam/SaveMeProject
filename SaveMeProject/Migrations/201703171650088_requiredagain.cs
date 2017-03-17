ecbbc6f3ba3e56cd793958c2d094b6b43188c00f
Jen Krejsa <jlkrejsa@gmail.com>
2017-03-17 13:35:42 -0400

    changes moved from master

---
+++ SaveMeProject/Migrations/201703171650088_requiredagain.cs
@@ -0,0 +1,18 @@
+namespace SaveMeProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class requiredagain : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.ReportAbusers", "AbuseDetails", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.ReportAbusers", "AbuseDetails", c => c.String());
+        }
+    }
+}
